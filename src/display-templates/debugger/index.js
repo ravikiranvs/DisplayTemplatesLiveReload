23279d02622c9d95ed8a64a385e5c4a92bddc900
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-08-25 12:00:15 +0530

    added eslink

--- src/display-templates/debugger/index.js
+++ src/display-templates/debugger/index.js
@@ -3,11 +3,11 @@ import DebuggerTemplate from './template.js';
 window.NamespacedDebugTemplateRenderer = function (ctx) {
   var items = ctx.ListData.ResultTables[0].ResultRows.map((item) => {
     var properties = Object.keys(item).map((key) => {
-      return { key, value: JSON.stringify(item[key]) }
+      return { key, value: JSON.stringify(item[key]) };
     });
     return { properties };
   });
 
   const debuggerTemplate = new DebuggerTemplate(window);
   return debuggerTemplate.renderAsString({ items });
-}
+};
