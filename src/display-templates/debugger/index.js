c2a4b32bcd0932c99f8d19ed71977393434fc4dd
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-08-25 11:34:00 +0530

    removing MDS. Display templates implement it themselves.

--- src/display-templates/debugger/index.js
+++ src/display-templates/debugger/index.js
@@ -1,18 +1,13 @@
 import DebuggerTemplate from './template.js';
-import MDS from '../../MDS.js';
 
-const DebuggerTemplateEntry = () => {
-  window.NamespacedDebugTemplateRenderer = function (ctx) {
-    var items = ctx.ListData.ResultTables[0].ResultRows.map((item) => {
-      var properties = Object.keys(item).map((key) => {
-        return { key, value: JSON.stringify(item[key]) }
-      });
-      return { properties };
+window.NamespacedDebugTemplateRenderer = function (ctx) {
+  var items = ctx.ListData.ResultTables[0].ResultRows.map((item) => {
+    var properties = Object.keys(item).map((key) => {
+      return { key, value: JSON.stringify(item[key]) }
     });
+    return { properties };
+  });
 
-    const debuggerTemplate = new DebuggerTemplate(window);
-    return debuggerTemplate.renderAsString({ items });
-  }
-};
-
-MDS('/Style%20Library/scripts/debugging-display-template.bundle.js', DebuggerTemplateEntry);
+  const debuggerTemplate = new DebuggerTemplate(window);
+  return debuggerTemplate.renderAsString({ items });
+}
